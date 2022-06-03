5a0fc13eaf63821549d52b44fb1e121942a108cc
Chiamakaj <juliet.ohaji@gmail.com>
2022-06-02 17:33:10 -0700

    importing the file to be read

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import  hidden_4.pyc
+    import sys
+    import  hidden_4
     for a in dir(hidden_4):
         if a[:2] != "__":
-            print("{:a}".format(a))
+            print("{:s}".format(a))
