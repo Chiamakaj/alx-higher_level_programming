b14a07c21eea2f8296a97af8f3467f2c98cf8c27
Chiamakaj <juliet.ohaji@gmail.com>
2022-06-05 02:58:13 -0700

    PEP8 correction

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,9 +3,9 @@ def replace_in_list(my_list, idx, element):
     for len in my_list:
         len += 0
     if idx < 0 or idx > len:
-        return(my_list)
+        return (my_list)
     else:
         for i in range(0, len):
             if i == idx:
                 my_list[i] = element
-                return(my_list)
+                return (my_list)
