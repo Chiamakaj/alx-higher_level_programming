f3dbe3a46fcd8e298b1d9bfd8773bf0733c4cd2f
Chiamakaj <juliet.ohaji@gmail.com>
2022-06-08 16:18:13 -0700

    code correction

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if not matrix:
-        print ()
-    return [list(map(lambda x : x * x, obj)) for obj in matrix]
+        print()
+    return [[elm ** 2 for elm in row] for row in matrix]
