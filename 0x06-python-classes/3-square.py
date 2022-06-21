63c524abc97d711e5be6cefb65af002057a84800
Chiamakaj <juliet.ohaji@gmail.com>
2022-06-21 15:36:23 -0700

    adding a class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Create a class called square"""
+
+
+class Square:
+    """Define Square Size.
+
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size=0):
+        """Initializes the square
+
+        Args:
+            size (int): size of one side of the square
+
+        Returns:
+            None
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+    def area(self):
+        """Calculates the area of a square
+
+        Return:
+            Area of square
+        """
+        return self.__size ** 2
