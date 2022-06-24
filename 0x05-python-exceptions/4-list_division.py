866a3b9a029e0716a96adacd361c538c94aa2307
Chiamakaj <juliet.ohaji@gmail.com>
2022-06-24 07:13:33 -0700

    adding function

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -15,4 +15,4 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         finally:
             my_new_list.append(result)
-    return  my_new_list
+    return my_new_list
