4cfe9c639cc0e153bbc22383fafb8ffd395a9954
Chiamakaj <juliet.ohaji@gmail.com>
2022-07-06 08:15:41 -0700

    Adding a function

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Contains the lookup function
+"""
+
+def lookup(obj):
+    """
+    Returns the list containing attributes
+    and methods of an object
+    """
+    return dir(obj)
