02d2dc487fff630c3807f059e24a269a8f1c0ad0
Chiamakaj <juliet.ohaji@gmail.com>
2022-07-07 11:15:11 -0700

    pep8

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,7 +9,7 @@ def write_file(filename="", text=""):
     A function that writes a string to a text file (UTF8)
     and returns the number of characters written
     """
-    with open(filename, mode = "w", encoding = "utf-8") as newfile:
+    with open(filename, mode="w", encoding="utf-8") as newfile:
         i = 0
         for word in newfile:
             for char in word:
