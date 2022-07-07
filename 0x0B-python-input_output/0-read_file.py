6ac816cdafe75203c025b99d1307400254c55c8e
Chiamakaj <juliet.ohaji@gmail.com>
2022-07-07 11:08:21 -0700

    pepe8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ A function that reads a text file
 
 def read_file(filename=""):
     """A function that reads a text file (UTF8) and prints it to stdout"""
-    with open('filename', mode = "r", encoding = "utf-8") as newfile:
+    with open(filename, mode="r", encoding="utf-8") as newfile:
         print(newfile.read(), end="")
