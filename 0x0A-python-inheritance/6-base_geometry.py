8eefe6e2fd8790a080e0d8b98d9c56af5771b823
Chiamakaj <juliet.ohaji@gmail.com>
2022-07-07 06:08:08 -0700

    Adding class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+A class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """An empty class"""
+    def __init__(self):
+        """Initializes the class"""
+        pass
+    def area(self):
+        """raises an exception"""
+        raise Exception("area() is not implemented")
