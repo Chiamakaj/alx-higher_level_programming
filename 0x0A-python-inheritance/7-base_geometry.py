3607f6b21c842ea83adb0434f7655ff6af1aee7f
Chiamakaj <juliet.ohaji@gmail.com>
2022-07-07 06:26:02 -0700

    code correction

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,10 +6,6 @@ A class BaseGeometry
 
 class BaseGeometry:
     """An empty class"""
-    def __init__(self):
-        """Initializes the class"""
-        pass
-
     def area(self):
         """raises an exception"""
         raise Exception("area() is not implemented")
