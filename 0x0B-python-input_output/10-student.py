b0406d196ec50d8f9b31cf9e22e30de3c48ebf3e
Chiamakaj <juliet.ohaji@gmail.com>
2022-07-08 13:22:35 -0700

    adding class

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A class called student
+"""
+
+
+class Student:
+    """A class that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation of the class"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        if attrs == None or type(attrs) != list:
+            return self.__dict__
+        else:
+            temp = {}
+            for elem in attrs:
+                if type(elem) != str:
+                    return self.__dict__
+                if elem in self.__dict__.keys():
+                    temp[elem] = self.__dict__[elem]
+            return temp
